115d62c766c3b85e251e1540d5249afa7e0efab3
Martin <m87.wheeler@gmail.com>
2025-10-22 22:09:41 +0100

    Feat/apis (#3)
    * wip(web-v1, redis): setup initial redis client
    
    * feat(web-v1, api): add api endpoints

--- apps/web-v1/app/api/membership/route.ts
+++ apps/web-v1/app/api/membership/route.ts
@@ -1,7 +1,27 @@
-export const GET = async (_: Request): Promise<Response> => {
+import { RedisClient } from "@/libs/redis";
+import { DBMember } from "@ved-poc-monorepo/shared";
+
+export const GET = async (req: Request): Promise<Response> => {
   try {
-    return new Response("Membership info retrieved", { status: 200 });
+    const body = await req.json();
+    const { memberId } = body;
+
+    if (!memberId) {
+      return new Response("Bad Request: memberId is required", { status: 400 });
+    }
+
+    const redis = new RedisClient<DBMember>();
+    await redis.connect();
+
+    const member = await redis.get(memberId);
+    await redis.disconnect();
+    if (!member) {
+      return new Response("Member not found", { status: 404 });
+    }
+
+    return Response.json(member, { status: 200 });
   } catch (error) {
+    console.error("Error retrieving member:", error);
     return new Response("Internal Server Error", { status: 500 });
   }
-}
+};
